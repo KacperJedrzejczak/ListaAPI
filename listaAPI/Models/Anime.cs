0511eb9cc95b93dc186c0583af8effa4bad99ed9
Kacper Jędrzejczak <jotkacpi@gmail.com>
2024-06-04 01:55:46 +0200

    Dziala

---
+++ listaAPI/Models/Anime.cs
@@ -0,0 +1,9 @@
+﻿namespace AnimeAPI.Models
+{
+    public class Anime
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Status { get; set; } // "Watched", "Watching", "Planned"
+    }
+}
---
+++ listaAPI/Services/JsonServices.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using AnimeAPI.Models;
+using Newtonsoft.Json;
+
+namespace YourProjectName.Services
+{
+    public class AnimeService
+    {
+        private readonly string _filePath;
+
+        public AnimeService(string filePath)
+        {
+            _filePath = filePath;
+        }
+
+        public List<Anime> GetAnimeList()
+        {
+            // Wczytaj dane z pliku JSON
+            string json = File.ReadAllText(_filePath);
+
+            // Deserializuj dane JSON do listy obiektów Anime
+            List<Anime> animeList = JsonConvert.DeserializeObject<List<Anime>>(json);
+
+            return animeList;
+        }
+    }
+}
